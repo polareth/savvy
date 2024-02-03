2e91ac495e9528ac18e80eb4fdf6bd61ee98f05b
polarzero <0xpolarzero@gmail.com>
2024-02-03 19:03:47 +0100

    Extend base config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack5: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
 
 export default nextConfig;
