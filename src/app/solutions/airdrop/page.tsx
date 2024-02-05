b1dd3ec0bfa2876668b53c016e071d62137dc572
polarzero <0xpolarzero@gmail.com>
2024-02-05 19:02:31 +0100

    Move to subfolder

--- src/app/solutions/airdrop/page.tsx
+++ src/app/solutions/airdrop/page.tsx
@@ -1,8 +1,8 @@
-import AirdropMethodSelection from '@/components/pages/solutions/airdrop-method-selection';
+import AirdropMethodSelection from '@/components/pages/solutions/airdrop/airdrop-method-selection';
+import TokenSelection from '@/components/pages/solutions/airdrop/token-selection';
 import ChainSelection from '@/components/pages/solutions/chain-selection';
 import GasPriceSelection from '@/components/pages/solutions/gas-price-selection';
 import NativePriceSelection from '@/components/pages/solutions/native-price-selection';
-import TokenSelection from '@/components/pages/solutions/token-selection';
 
 const Airdrop = () => {
   return (
