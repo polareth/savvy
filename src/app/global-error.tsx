cd6c662c57b4e78ab6629dc0d4ef2ff0176a017e
polarzero <0xpolarzero@gmail.com>
2024-03-22 11:12:44 +0100

    :bug: Add local storage reset in global-error to handle breaking changes

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -7,6 +7,8 @@ import '@/styles/globals.css';
 
 /**
  * @notice A fallback component to handle global errors
+ * @dev Some breaking changes during early development might incur some irreversible errors;
+ * the solution here is to clear the local storage and reset the application.
  * @param error - The error that was thrown
  * @param reset - A function to reset the application
  * @returns The error message and a button to reset the application
@@ -25,15 +27,25 @@ const GlobalError = ({
           <div className="flex flex-col gap-4">
             <h2 className="text-lg font-medium">Something went wrong</h2>
             <p className="text-muted-foreground">
-              An error occurred while rendering the application.
+              An error occurred while rendering the application. This might be
+              due to a breaking change.
             </p>
             <p className="text-xs text-muted-foreground">
               {error.digest
                 ? `Error digest: ${error.digest}`
                 : `Error message: ${error.message}`}
             </p>
-            <Button type="reset" onClick={() => reset()}>
-              Try again
+            <p className="font-medium text-muted-foreground">
+              You can try to clear the local storage to reset the application.
+            </p>
+            <Button
+              type="reset"
+              onClick={() => {
+                localStorage.clear();
+                reset();
+              }}
+            >
+              Reset application
             </Button>
           </div>
         </ContainerLayout>
